34d735a53a001156a9cc3ce8975b158a7d8cdde4
uillas <uillasreis@hotmail.com>
2023-08-23 21:00:47 -0300

    add Maps

--- src/Components/ComponentsDetails/Maps.js
+++ src/Components/ComponentsDetails/Maps.js
@@ -1,56 +1,64 @@
-
-
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-
-
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const containerStyle = {
     width: '100%',
     height: '100%',
 };
 
-const center = {
-    lat: -23.4837230553292, 
-    lng: -46.80522626147561
-};
+function MyComponent({ location }) {
+    const [center, setCenter] = useState(null);
 
-function MyComponent() {
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "AIzaSyDFg4EHK-9CaKFqmqqSivcc64Xzi-YEk3M"
-    })
+    // Função para abrir o Google Maps ao clicar no botão
+    const handleOpenGoogleMaps = () => {
+        if (center) {
+            const { lat, lng } = center;
+            const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+            window.open(url, '_blank');
+        }
+    };
+    // Função para buscar as coordenadas da cidade a partir do endereço
+    useEffect(() => {
+        const fetchCityCoordinates = async () => {
+            try {
+                const response = await fetch(
+                    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+                        location,
+                    )}&key=${googleMapsApiKey}`,
+                );
+                if (response.ok) {
+                    const data = await response.json();
+                    const { lat, lng } = data.results[0].geometry.location;
+                    setCenter({ lat, lng });
+                } else {
+                    throw new Error('Failed to fetch city coordinates');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-    const [map, setMap] = React.useState(null)
+        if (location) {
+            fetchCityCoordinates();
+        }
+    }, [location]);
 
-    const onLoad = React.useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-
-        setMap(map)
-    }, [])
-
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
-    return isLoaded ? (
-        <div className="flex h-[460px] w-full flex-col ">
+    return (
+        <div className="flex h-[460px] w-full flex-col">
             {center && (
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={15}
-                >
-                    {/* Child components, such as markers, info windows, etc. */}
-                    <Marker position={center} />
-                </GoogleMap>
+                <LoadScript googleMapsApiKey={googleMapsApiKey} >
+                    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+                        <Marker position={center} />
+                    </GoogleMap>
+                </LoadScript>
             )}
-            <button variant="outlined">
+            <button className='bg-cyan-700 hover:bg-cyan-600 text-white cursor-pointer rounded-lg mt-4 h-10'
+                onClick={handleOpenGoogleMaps} variant="outlined">
                 Ver no Google Maps
             </button>
         </div>
-    ) : <></>
+    );
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MyComponent);
