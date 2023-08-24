a2fc0c734b945fbfed84dc1f765f525c8d3652da
uillas <uillasreis@hotmail.com>
2023-08-24 18:42:51 -0300

    Maps

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tailwindcss/tailwind.css';
 import Routes from './Routes';
+import { UserProvider } from './hooks/UserContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-
-    <Routes />
-
+    <UserProvider >
+      <Routes />
+    </UserProvider>
 
   </>
 );
