f6b8435e937dae1105dc2e62631ff1a25711131e
uillas <uillasreis@hotmail.com>
2023-11-08 20:10:35 -0300

    cadastrar um novo usuário

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import api from "../services/api";
 import Modal from "react-modal";
 import Register from "./Register";
@@ -11,144 +11,156 @@ import { useHistory } from "react-router-dom";
 import paths from "./Admin/Paths";
 
 const customStyles = {
-    overlay: {
-        backgroundColor: "rgba(0, 0, 0, 0.7)",
-        zIndex: 1000,
-    },
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        transform: "translate(-50%, -50%)",
-        borderRadius: "8px",
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-        maxWidth: "400px",
-        width: "90%",
-        padding: "20px",
-        background: "white",
-    },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    zIndex: 1000,
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "8px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+    maxWidth: "400px",
+    width: "90%",
+    padding: "20px",
+    background: "white",
+  },
 };
 
 function Login({ isOpen, onRequestClose, errorMessage }) {
-    const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
-    const { login } = useUser()
-    const [error, setError] = useState("");
-    const history = useHistory()
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+  const { login } = useUser();
+  const [error, setError] = useState("");
+  const history = useHistory();
 
-    const schema = yup.object().shape({
-        email: yup.string().email('Digite um e-mail válido')
-            .required('O e-mail é obrigatório'),
-        password: yup.string().required('A senha e obrigatória')
-            .min(6, 'A senha deve ter pelo 6 digitos')
-    })
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Digite um e-mail válido")
+      .required("O e-mail é obrigatório"),
+    password: yup
+      .string()
+      .required("A senha e obrigatória")
+      .min(6, "A senha deve ter pelo 6 digitos"),
+  });
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm({
-        resolver: yupResolver(schema)
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const onSubmit = async (clientData) => {
-        try {
-          const { data } = await api.post("login", {
-            email: clientData.email,
-            password: clientData.password,
-            
-          });
-          login(data);
-         
-          if (data.user.admin) {
-            history.push(paths.Reservations); 
-         
-          } else {
-            history.push("/");
-          }
-        } catch (error) {
-          setError("Usuário não encontrado");
-        }
-      };
+  const onSubmit = async (clientData) => {
+    try {
+      const { data } = await api.post("login", {
+        email: clientData.email,
+        password: clientData.password,
+      });
+      login(data);
 
-    const openRegisterModal = () => {
-        setIsRegisterModalOpen(true);
-    };
+      if (data.user.admin) {
+        history.push(paths.Reservations);
+      } else {
+        history.push("/");
+      }
+    } catch (error) {
+      setError("Usuário não encontrado");
+    }
+  };
 
-    const closeRegisterModal = () => {
-        setIsRegisterModalOpen(false);
-    };
+  const openRegisterModal = () => {
+    setIsRegisterModalOpen(true);
+  };
 
+  const closeRegisterModal = () => {
+    setIsRegisterModalOpen(false);
+  };
 
-    return (
-
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            style={customStyles}
-            ariaHideApp={false}
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+      ariaHideApp={false}
+    >
+      <div className="text-right ">
+        <button
+          onClick={onRequestClose}
+          className="text-gray-500 hover:text-gray-700 text-2xl"
         >
-            <div className="text-right ">
-                <button onClick={onRequestClose} className="text-gray-500 hover:text-gray-700 text-2xl">
-                    &times;
-                </button>
-            </div>
-            <h2 className="text-2xl font-bold text-gray-700 text-center mb-4">Faça Login</h2>
-            <span className="flex mt-1 justify-center text-xs text-center text-red-500">{error}</span>
-            <span className="flex mt-1 justify-center text-xs text-center text-red-500">{errorMessage}</span>
-            <form noValidate onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-1">
-                        Email:
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        {...register('email')}
-                        /*    onChange={handleEmailInputChange}  */
-                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-cyan-700 shadow-sm"
-                        placeholder="Seu email"
-                        required
-                    />
-                    <p className="errors">{errors.email?.message}</p>
+          &times;
+        </button>
+      </div>
+      <h2 className="text-2xl font-bold text-gray-700 text-center mb-4">
+        Faça Login
+      </h2>
+      <span className="flex mt-1 justify-center text-xs text-center text-red-500">
+        {error}
+      </span>
+      <span className="flex mt-1 justify-center text-xs text-center text-red-500">
+        {errorMessage}
+      </span>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-medium mb-1"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            {...register("email")}
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-cyan-700 shadow-sm"
+            placeholder="Seu email"
+            required
+          />
+          <p className="errors">{errors.email?.message}</p>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-medium mb-1"
+          >
+            Senha:
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register("password")}
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-cyan-700 shadow-sm"
+            placeholder="Sua senha"
+            required
+          />
+          <p className="errors">{errors.password?.message}</p>
+        </div>
+        <Button type="submit">Entrar</Button>
+      </form>
+      <div className="text-center mt-4">
+        <p className="text-gray-600">
+          Ainda não tem uma conta?{" "}
+          <span
+            className="text-cyan-700 cursor-pointer"
+            onClick={openRegisterModal}
+          >
+            Cadastre-se
+          </span>
+        </p>
+      </div>
 
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="password" className="block text-gray-700 text-sm font-medium mb-1">
-                        Senha:
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        {...register('password')}
-                        /*    onChange={handlePasswordInputChange} */
-                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-cyan-700 shadow-sm"
-                        placeholder="Sua senha"
-                        required
-                    />
-                    <p className="errors">{errors.password?.message}</p>
-
-                </div>
-                <Button
-                    type="submit"
-                /*  className="w-full bg-cyan-700 text-white py-2 rounded-lg hover:bg-cyan-700"  */
-                >
-                    Entrar
-                </Button>
-            </form>
-            <div className="text-center mt-4">
-                <p className="text-gray-600">
-                    Ainda não tem uma conta?{' '}
-                    <span className="text-cyan-700 cursor-pointer" onClick={openRegisterModal}>
-                        Cadastre-se
-                    </span>
-                </p>
-            </div>
-
-            <Register isOpen={isRegisterModalOpen} onRequestClose={closeRegisterModal} />
-        </Modal>
-
-    );
+      <Register
+        isOpen={isRegisterModalOpen}
+        onRequestClose={closeRegisterModal}
+        onRegisterSuccess={closeRegisterModal}
+      />
+    </Modal>
+  );
 }
 
 export default Login;
--- src/Containers/Register.js
+++ src/Containers/Register.js
@@ -1,108 +1,198 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Button from "../Components/Button";
+import api from "../services/api";
 
 const customStyles = {
-    overlay: {
-        backgroundColor: "transparent",
-        zIndex: 1000,
-    },
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        transform: "translate(-50%, -50%)",
-        borderRadius: "8px",
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-        maxWidth: "400px",
-        width: "90%", 
-        padding: "20px",
-        background: "white",
-    },
+  overlay: {
+    backgroundColor: "transparent",
+    zIndex: 1000,
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "8px",
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+    maxWidth: "400px",
+    width: "90%",
+    padding: "20px",
+    background: "white",
+  },
 };
 
-/* function Register({ isOpen, onRequestClose }) { */
-function Register({ isOpen, onRequestClose }) {
-    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+function Register({ isOpen, onRequestClose, onRegisterSuccess }) {
+  const [error, setError] = useState("");
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
-    const openLoginModal = () => {
-        setIsLoginModalOpen(true);
-        onRequestClose(); // Feche o modal de registro ao abrir o modal de login
-    };
+  const schema = yup.object().shape({
+    name: yup.string().required("O nome é obrigatório"),
+    email: yup
+      .string()
+      .email("Digite um e-mail válido")
+      .required("O e-mail é obrigatório"),
+    password: yup
+      .string()
+      .required("A senha é obrigatória")
+      .min(6, "A senha deve ter pelo menos 6 dígitos"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "As senhas devem coincidir"),
+  });
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    onRequestClose(); // Feche o modal de registro ao abrir o modal de login
+  };
 
+  const onSubmit = async (userData) => {
+    try {
+      if (userData.password !== userData.confirmPassword) {
+        setError("As senhas não coincidem");
+        return;
+      }
 
+      const response = await api.post("cadastre-se", {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        confirmPassword: userData.confirmPassword,
+      });
+      console.log(response);
+      if (response.status === 201) {
 
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            style={customStyles}
-            ariaHideApp={false}
+        onRegisterSuccess();
+      } else {
+        setError(
+          "Erro ao realizar o registro. Verifique suas informações e tente novamente."
+        );
+      }
+    } catch (error) {
+      setError(
+        "Erro ao realizar o registro. Verifique suas informações e tente novamente."
+      );
+    }
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+      ariaHideApp={false}
+    >
+      <div className="text-right">
+        <button
+          onClick={onRequestClose}
+          className="text-gray-500 hover-text-gray-700 text-2xl"
         >
-            <div className="text-right ">
-                <button onClick={onRequestClose} className="text-gray-500 hover:text-gray-700 text-2xl">
-                    &times;
-                </button>
-            </div>
-            <h2 className="text-2xl font-bold text-gray-700 text-center mb-4">Cadastre-se</h2>
-            <div className="mb-4">
-                <label htmlFor="nome" className="block text-gray-700 text-sm font-medium mb-1">
-                    Nome:
-                </label>
-                <input
-                    type="text"
-                    id="nome"
-                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 shadow-sm"
-                    placeholder="Seu nome"
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-1">
-                    Email:
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 shadow-sm"
-                    placeholder="Seu email"
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label htmlFor="password" className="block text-gray-700 text-sm font-medium mb-1">
-                    Senha:
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 shadow-sm"
-                    placeholder="Sua senha"
-                    required
-                />
-            </div>
-            <button
-                type="button"
-                className="w-full bg-cyan-700 text-white py-2 rounded-lg hover:bg-cyan-700"
-            >
-                Entrar
-            </button>
-            
-            <div className="text-center mt-4">
-                <p className="text-gray-600">
-                    Já tem uma conta?{' '}
-                    <span className="text-cyan-700 cursor-pointer" onClick={openLoginModal}>
-                        Faça Login
-                    </span>
-                </p>
-            </div>
+          &times;
+        </button>
+      </div>
+      <h2 className="text-2xl font-bold text-gray-700 text-center mb-4">
+        Cadastre-se
+      </h2>
+      <span className="flex mt-1 justify-center text-xs text-center text-red-500">
+        {error}
+      </span>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-gray-700 text-sm font-medium mb-1"
+          >
+            Nome:
+          </label>
+          <input
+            type="text"
+            id="name"
+            {...register("name")}
+            className="w-full px-4 py-2 border rounded-lg focus-outline-none focus-border-blue-500 shadow-sm"
+            placeholder="Seu nome"
+            required
+          />
+          <p className="errors">{errors.name?.message}</p>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-medium mb-1"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            {...register("email")}
+            className="w-full px-4 py-2 border rounded-lg focus-outline-none focus-border-blue-500 shadow-sm"
+            placeholder="Seu email"
+            required
+          />
+          <p className="errors">{errors.email?.message}</p>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-medium mb-1"
+          >
+            Senha:
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register("password")}
+            className="w-full px-4 py-2 border rounded-lg focus-outline-none focus-border-blue-500 shadow-sm"
+            placeholder="Sua senha"
+            required
+          />
+          <p className="errors">{errors.password?.message}</p>
+        </div>
 
-
-        </Modal>
-    );
+        <div className="mb-4">
+          <label
+            htmlFor="confirmPassword"
+            className="block text-gray-700 text-sm font-medium mb-1"
+          >
+            Confirmar Senha:
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            {...register("confirmPassword")}
+            className="w-full px-4 py-2 border rounded-lg focus-outline-none focus-border-blue-500 shadow-sm"
+            placeholder="Confirme sua senha"
+            required
+          />
+          <p className="errors">{errors.confirmPassword?.message}</p>
+        </div>
+        <Button type="submit">Cadastrar</Button>
+      </form>
+      <div className="text-center mt-4">
+        <p className="text-gray-600">
+          Já tem uma conta?{" "}
+          <span
+            className="text-cyan-700 cursor-pointer"
+            onClick={openLoginModal}
+          >
+            Faça Login
+          </span>
+        </p>
+      </div>
+    </Modal>
+  );
 }
 
 export default Register;
