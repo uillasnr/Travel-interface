32e53e208ff63758cce5703944cb6bf717c87775
uillas <uillasreis@hotmail.com>
2023-11-08 20:11:43 -0300

    Rotas privadas

--- src/Routes/private-routes.js
+++ src/Routes/private-routes.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
+import Paths from "../Containers/Admin/Paths";
+import { useUser } from "../hooks/UserContext";
 
-function PrivateRoute({ component, ...rest }) {
-    const user = localStorage.getItem("Travel:userData")
+function PrivateRoute({ component: Component, isAdmin, ...rest }) {
+  const { user } = useUser(); 
 
-    if (!user) {
-        return (
-            <Redirect to="/" />
-        )
-    }
+  
+  if (!user) {
+    return <Redirect to="/" />;
+  }
 
-    return <Route {...rest} component={component} />
+  // Verifique se o usuário é um administrador e está autorizado a acessar rotas privadas
+  if (isAdmin && !user.user.admin) {
+    return <Redirect to={Paths.Reservations} />;
+  }
+
+  // Se o usuário estiver autenticado e atender aos requisitos, renderize o componente
+  return <Route {...rest} render={(props) => <Component {...props} />} />;
 }
-
-export default PrivateRoute
 
 PrivateRoute.propTypes = {
-    component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
-}
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+  isAdmin: PropTypes.bool,
+};
+
+export default PrivateRoute;
